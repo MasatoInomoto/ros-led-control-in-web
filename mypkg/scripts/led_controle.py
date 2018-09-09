ebd90457522130bf2c09d71fc52dd165f297706e
KobayashiRui <roboroborx782@gmail.com>
2018-09-09 21:14:49 +0900

    update?

---
+++ mypkg/scripts/led_controle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import rospy
+import std_msgs.msg
+
+GPIO.setmode(GPIO.BOARD)
+gpio_list=[15,16]
+for i in range(2):
+    GPIO.setup(gpio_list[i], GPIO.OUT)
+
+def callback(data, id):
+  rospy.loginfo("[ID:"+str(id)+"] : " + str(data.data))
+  if(data.data==1):
+    GPIO.output(gpio_list[id-1],True)
+  else:
+    GPIO.output(gpio_list[id-1],False)
+
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+
+    led1 = rospy.Subscriber("/led1", std_msgs.msg.Int8, callback, callback_args=1)
+    led2 = rospy.Subscriber("/led2", std_msgs.msg.Int8, callback, callback_args=2)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
